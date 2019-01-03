fe1e90c8b5b466364db7db30b9d485215c0633d2
Oliver Joseph Ash <oliverjash@gmail.com>
2019-01-03 13:55:45 +0000

    Document fn

--- src/helpers/maybe.ts
+++ src/helpers/maybe.ts
@@ -5,6 +5,10 @@ type Maybe<T> = undefined | T;
 
 const isMaybeDefined = <T>(maybeT: Maybe<T>): maybeT is T => maybeT !== undefined;
 
+/** Takes a dictionary containing `Maybe`s and returns a dictionary of all the non-empty values. */
+// Inspired by
+// http://hackage.haskell.org/package/base-4.12.0.0/docs/Data-Maybe.html#v:catMaybes
+// https://github.com/gcanti/fp-ts/blob/42870714ebcae4ecf132291c687c845b6837b7d2/docs/Record.md#compact
 export const catMaybesDictionary = <T>(maybesDictionary: {
     [index: string]: Maybe<T>;
 }): { [index: string]: T } => pickBy(maybesDictionary, isMaybeDefined);
