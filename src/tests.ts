da0254943dc1b41e071a35683fd86d75448ca421
editkid <github.com@editkid.com>
2019-10-16 11:30:03 +1300

    Add tests

--- src/tests.ts
+++ src/tests.ts
@@ -1,8 +1,10 @@
 import * as assert from 'assert';
 import { buildImgixUrl } from './index';
 
+const baseUrl = 'https://foo.com';
+
 assert.strictEqual(
-    buildImgixUrl('https://foo.com')({
+    buildImgixUrl(baseUrl)({
         auto: {
             format: true,
         },
@@ -10,3 +12,30 @@ assert.strictEqual(
     }),
     'https://foo.com/?auto=format&w=300',
 );
+
+const invalidRatios = ['foo', '1:', 'foo:1', '1:foo'];
+
+const validRatios = ['1:1', '1.91:1', '1.91:1.28'];
+
+assert.strictEqual(
+    buildImgixUrl(baseUrl)({
+        ar: '1:1',
+    }),
+    'https://foo.com/?ar=1%3A1',
+);
+
+invalidRatios.forEach(ratio => {
+    assert.throws(() =>
+        buildImgixUrl(baseUrl)({
+            ar: ratio,
+        }),
+    );
+});
+
+validRatios.forEach(ratio => {
+    assert.doesNotThrow(() =>
+        buildImgixUrl(baseUrl)({
+            ar: ratio,
+        }),
+    );
+});
