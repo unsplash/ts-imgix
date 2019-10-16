0c53d88f5146c592122b59748d2609edeb3bc7c6
editkid <github.com@editkid.com>
2019-10-17 10:20:41 +1300

    Remove redundant tests

--- src/tests.ts
+++ src/tests.ts
@@ -1,10 +1,8 @@
 import * as assert from 'assert';
 import { buildImgixUrl } from './index';
 
-const baseUrl = 'https://foo.com';
-
 assert.strictEqual(
-    buildImgixUrl(baseUrl)({
+    buildImgixUrl('https://foo.com')({
         auto: {
             format: true,
         },
@@ -12,30 +10,3 @@ assert.strictEqual(
     }),
     'https://foo.com/?auto=format&w=300',
 );
-
-const invalidRatios = ['foo', '1:', 'foo:1', '1:foo'];
-
-const validRatios = ['1:1', '1.91:1', '1.91:1.28', '1:1.28546'];
-
-assert.strictEqual(
-    buildImgixUrl(baseUrl)({
-        ar: '1:1',
-    }),
-    'https://foo.com/?ar=1%3A1',
-);
-
-invalidRatios.forEach(ratio => {
-    assert.throws(() =>
-        buildImgixUrl(baseUrl)({
-            ar: ratio,
-        }),
-    );
-});
-
-validRatios.forEach(ratio => {
-    assert.doesNotThrow(() =>
-        buildImgixUrl(baseUrl)({
-            ar: ratio,
-        }),
-    );
-});
