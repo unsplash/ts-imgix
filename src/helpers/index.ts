2440cce7ac47f8046e93faacfd60c75396b01115
Oliver Joseph Ash <oliverjash@gmail.com>
2019-10-18 15:50:21 +0100

    Uprade TypeScript from 3.2.2 to 3.6.4

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,5 +1,3 @@
-const tuple = <T extends Array<unknown>>(...args: T): T => args;
-
 // `Object.keys` does not return the keys as string literals, only strings. Use this helper as a
 // workaround. https://github.com/Microsoft/TypeScript/pull/12253#issuecomment-263132208
 const keys = <O extends object>(obj: O) => Object.keys(obj) as Array<keyof O>;
@@ -7,7 +5,7 @@ const keys = <O extends object>(obj: O) => Object.keys(obj) as Array<keyof O>;
 // `Object.entries` is ES2017, but this lib only supports ES2015 at runtime, so we must define our
 // own version.
 const entries = <K extends string, V>(obj: Record<K, V>) =>
-    keys(obj).map(key => tuple(key, obj[key]));
+    keys(obj).map(key => [key, obj[key]] as const);
 const fromEntries = <K extends string, V>(arr: Array<[K, V]>) =>
     // `Object.assign` is poorly typed: it returns `any` when spreading. Use cast to workaround.
     Object.assign({}, ...arr.map(([k, v]) => ({ [k]: v }))) as Record<K, V>;
