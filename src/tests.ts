a53d6641c9803608e33f5a188ead68ae7c25ee4d
Angelo Ashmore <angeloashmore@users.noreply.github.com>
2020-04-15 18:29:13 -1000

    feat: s param and optional URL signing

--- src/tests.ts
+++ src/tests.ts
@@ -10,3 +10,24 @@ assert.strictEqual(
     }),
     'https://foo.com/?auto=format&w=300',
 );
+
+assert.strictEqual(
+    buildImgixUrl('https://foo.com')({
+        auto: {
+            format: true,
+        },
+        w: 300,
+        s: 'signature',
+    }),
+    'https://foo.com/?auto=format&w=300&s=signature',
+);
+
+assert.strictEqual(
+    buildImgixUrl('https://foo.com', 'token')({
+        auto: {
+            format: true,
+        },
+        w: 300,
+    }),
+    'https://foo.com/?auto=format&w=300&s=d82d76f9f31379083b452f98bcd7f670',
+);
